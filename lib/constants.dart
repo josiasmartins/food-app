c0527f2d8ef0c781ad007ee5b7b71addebcd61f8
Caitano <josiasmartinscaitano@gmail.com>
2022-04-10 17:14:49 -0300

    fixture: adicionado o HomeScreen

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,6 +6,6 @@ const kActiveColor = Color(0xFF22A45D);
 const kAccentColor = Color(0xFFEF9920);
 const kBodyTextColor = Color(0xFF868686);
 const kInputColor = Color(0xFFFBFBFB);
-const kBgColor = Color.white;
+const kBgColor = Colors.white;
 
 const double defaultPadding = 16.0;
---
+++ lib/demoData.dart
@@ -0,0 +1,38 @@
+// It contains all our demo data that we used 
+
+List<String> demoBigImages = [
+  "assets/images/big_1.png",
+  "assets/images/big_2.png",
+  "assets/images/big_3.png",
+];
+
+List<Map<String, dynamic>> demoMediumCarData = [
+  {
+    "name": "Daylight Coffe",
+    "image": "assets/images/medium_1.png",
+    "location": "Colorado, San Francisco",
+    "rating": 4.6,
+    "deliverTime": 25
+  },
+  {
+    "name": "Mario Italiano",
+    "image": "assets/images/medium_2.png",
+    "location": "Colorado, San Francisco",
+    "rating": 4.3,
+    "deliverTime": 25
+  },
+  {
+    "name": "McDonald's",
+    "image": "assets/images/medium_3.png",
+    "location": "Colorado, San Francisco",
+    "rating": 4.6,
+    "deliverTime": 25
+  },
+  {
+    "name": "Daylight Coffe",
+    "image": "assets/images/medium_1.png",
+    "location": "Colorado, San Francisco",
+    "rating": 4.6,
+    "deliverTime": 25
+  },
+];
